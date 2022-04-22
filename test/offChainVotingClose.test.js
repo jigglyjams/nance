80196ff46681c3bff10f3457b6e24e57d927e57f
jigglyjams <jigglyjams@protonmail.com>
2022-04-21 23:11:10 -0700

    Small offchainvotingclose test

---
+++ test/offChainVotingClose.test.js
@@ -0,0 +1,9 @@
+import * as nance from '../nance.js';
+
+nance.closeVotingOffChain()
+
+// import * as snapshot from '../snapshot.js';
+
+// const votes = await snapshot.getProposalVotes('0x5371f3d8cef87b3b827883b074bce1e972a913fc5e26da2fb33f873e683dc704')
+// console.log(votes);
+// console.log(nance.offChainVotePassCheck(votes));
